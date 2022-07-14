0574e1ad25d1d51dd00b4dc7e3e2fd1db10bb705
Seb <szuddas1@sheffield.ac.uk>
2022-07-14 15:46:22 +0100

    testing branch inspiration from other repo

---
+++ entities.js
@@ -0,0 +1,287 @@
+function createEntity(x, y, template) {
+    var e = new Entity(x, y);
+
+    // Define max nutrition from nutrition if necessary
+    if (
+        typeof template.nutrition !== 'undefined' &&
+        typeof template.maxNutrition === 'undefined'
+    ) {
+        e.maxNutrition = template.nutrition;
+    }
+    // Define nutrition from max nutrition if necessary
+    if (
+        typeof template.nutrition === 'undefined' &&
+        typeof template.maxNutrition !== 'undefined'
+    ) {
+        e.nutrition = template.maxNutrition;
+    }
+
+    // Fill in all keys
+    var keys = Object.keys(template);
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        e[key] = template[key];
+    }
+    e.template = template;
+    return e;
+}
+
+
+// Steering functions
+
+function nearestTarget(entities, newEntities) {
+    var sum = createVector(0, 0);
+
+    // Pursuing a single target
+    var targets = getByName(entities, this.toChase);
+    if (targets.length > 0) {
+        var e = this.getNearest(targets);
+        if (e !== this) {
+            if (chaseLines) {
+                if (lineMode) {
+                    stroke(255);
+                } else {
+                    stroke(this.color[0], this.color[1], this.color[2], 127);
+                }
+                line(e.pos.x, e.pos.y, this.pos.x, this.pos.y);
+            }
+            this.onChase(e, newEntities);
+            sum.add(this.target(e, this.chasePriority));
+        }
+    }
+
+    // Avoidance
+    targets = getByName(entities, this.toAvoid);
+    for (var i = 0; i < targets.length; i++) {
+        var e = targets[i];
+        if (e === this) continue;
+        if (avoidLines) {
+            if (lineMode) {
+                stroke(255);
+            } else {
+                stroke(0, 0, 255);
+            }
+            line(e.pos.x, e.pos.y, this.pos.x, this.pos.y);
+        }
+        this.onAvoid(e, newEntities);
+        sum.add(this.target(e, this.avoidPriority * -1));
+    }
+    
+    return sum;
+}
+
+function multiTarget(entities, newEntities) {
+    var sum = createVector(0, 0);
+
+    // Pursuing targets
+    var targets = getByName(entities, this.toChase);
+    for (var i = 0; i < targets.length; i++) {
+        var e = targets[i];
+        if (e === this) continue;
+        if (chaseLines) {
+            if (lineMode) {
+                stroke(255);
+            } else {
+                stroke(this.color[0], this.color[1], this.color[2], 191);
+            }
+            line(e.pos.x, e.pos.y, this.pos.x, this.pos.y);
+        }
+        this.onChase(e, newEntities);
+        sum.add(this.target(e, this.chasePriority));
+    }
+
+    // Avoidance
+    targets = getByName(entities, this.toAvoid);
+    for (var i = 0; i < targets.length; i++) {
+        var e = targets[i];
+        if (e === this) continue;
+        if (avoidLines) {
+            if (lineMode) {
+                stroke(255);
+            } else {
+                stroke(0, 0, 255);
+            }
+            line(e.pos.x, e.pos.y, this.pos.x, this.pos.y);
+        }
+        this.onAvoid(e, newEntities);
+        sum.add(this.target(e, this.avoidPriority * -1));
+    }
+
+    return sum;
+}
+
+
+// Templates
+
+var templates = {};
+
+templates.food = {
+    accAmt: 0,
+    color: [135, 211, 124],
+    name: 'food',
+    topSpeed: 0,
+    hunger: function() {}
+};
+
+templates.fungus = {
+    accAmt: 0,
+    color: [102, 51, 153],
+    name: 'fungus',
+    nutrition: 500,
+    perception: 10,
+    radius: 10,
+    toChase: ['prey'],
+    toEat: ['prey'],
+    topSpeed: 0,
+    onEat: function(e, newEntities) {
+        if (this.eat(e)) {
+            if (random(2) < 1) {
+                var x = this.pos.x + random(-20, 20);
+                var y = this.pos.y + random(-20, 20);
+                newEntities.push(createEntity(x, y, templates.food));
+            }
+            var x = this.pos.x + random(-100, 100);
+            var y = this.pos.y + random(-100, 100);
+            newEntities.push(createEntity(x, y, templates.fungus));
+        }
+    }
+}
+
+templates.hive = {
+    accAmt: 0,
+    color: [54, 215, 183],
+    name: 'hive',
+    nutrition: 500,
+    perception: 100,
+    radius: 30,
+    steer: nearestTarget,
+    toChase: ['fungus', 'pred', 'prey'],
+    topSpeed: 0,
+    onChase: function(e, newEntities) {
+        if (random(15) >= 1) return;
+        var x = this.pos.x + random(-20, 20);
+        var y = this.pos.y + random(-20, 20);
+        var s = createEntity(x, y, templates.swarm);
+        s.hive = this;
+        newEntities.push(s);
+    }
+};
+
+templates.pred = {
+    accAmt: 0.4,
+    avoidPriority: 0.5,
+    chasePriority: 4,
+    color: [207, 0, 15],
+    name: 'pred',
+    nutrition: 200,
+    perception: 150,
+    radius: 12,
+    steer: multiTarget,
+    toAvoid: ['pred', 'swarm'],
+    toChase: ['prey'],
+    toEat: ['prey'],
+    topSpeed: 4,
+    onDeath: function(newEntities) {
+        if (random(3) >= 2) return;
+        var x = this.pos.x;
+        var y = this.pos.y;
+        newEntities.push(createEntity(x, y, templates.food));
+    },
+    onEatAttempt: function(e, newEntities) {
+        this.vel.mult(0);
+        if (random(5) >= 1) return;
+        if (this.onEat(e, newEntities)) e.onEaten(this, newEntities);
+    },
+    onEat: function(e, newEntities) {
+        if (this.eat(e)) {
+            if (random(5) >= 1) return false;
+            var x = this.pos.x + random(-20, 20);
+            var y = this.pos.y + random(-20, 20);
+            newEntities.push(createEntity(x, y, templates.pred));
+        }
+    }
+};
+
+templates.prey = {
+    accAmt: 0.5,
+    chasePriority: 2,
+    color: [82, 179, 217],
+    name: 'prey',
+    nutrition: 400,
+    perception: 100,
+    radius: 8,
+    steer: nearestTarget,
+    toChase: ['food'],
+    toEat: ['food'],
+    topSpeed: 3,
+    onEat: function(e, newEntities) {
+        if (this.eat(e)) {
+            var x = this.pos.x + random(-20, 20);
+            var y = this.pos.y + random(-20, 20);
+            newEntities.push(createEntity(x, y, templates.prey));
+        }
+    }
+};
+
+templates.swarm = {
+    accAmt: 0.4,
+    chasePriority: 4,
+    color: [249, 191, 59],
+    name: 'swarm',
+    nutrition: 150,
+    perception: 75,
+    steer: nearestTarget,
+    toAvoid: ['swarm'],
+    toChase: ['fungus', 'pred', 'prey'],
+    toEat: ['fungus', 'pred', 'prey'],
+    topSpeed: 4,
+    onChase: function(e, newEntities) {
+        if (random(5) >= 1) return;
+        var x = this.pos.x + random(-20, 20);
+        var y = this.pos.y + random(-20, 20);
+        var s = createEntity(x, y, templates.swarmer);
+        newEntities.push(s);
+    },
+    onDeath: function(newEntities) {
+        if (random(3) >= 2) return;
+        newEntities.push(createEntity(this.pos.x, this.pos.y, templates.food));
+    },
+    onEatAttempt: function(e, newEntities) {
+        if (typeof this.hive !== 'undefined' && !this.hive.alive) {
+            this.hive = undefined;
+        }
+        this.vel.mult(0);
+        if (random(15) >= 1) return;
+        var success;
+        if (typeof this.hive === 'undefined') {
+            success = this.onEat(e, newEntities);
+        } else {
+            success = this.hive.onEat(e, newEntities);
+        }
+        if (!success) return;
+        e.onEaten(this, newEntities);
+        if (random(23) >= 1) return;
+        newEntities.push(createEntity(this.pos.x, this.pos.y, templates.hive));
+        if (typeof this.hive !== 'undefined') return;
+        newEntities.push(createEntity(
+            this.pos.x, this.pos.y, templates.swarm
+        ));
+    }
+};
+
+templates.swarmer = {
+    accAmt: 0.4,
+    color: [249, 191, 59],
+    name: 'swarmer',
+    nutrition: 25,
+    perception: 50,
+    steer: nearestTarget,
+    toChase: ['fungus', 'pred', 'prey'],
+    toEat: ['fungus', 'pred', 'prey'],
+    topSpeed: 4,
+    onEatAttempt: function(e, newEntities) {
+        this.vel.mult(0);
+        if (random(15) >= 1) return;
+        if (this.onEat(e, newEntities)) e.onEaten(this, newEntities);
+    }
+};
--- predator_prey.js
+++ predator_prey.js
@@ -1,8 +1,374 @@
-function setup() {
+var entities;
+var newEntities;
+var selected = 'prey';
 
+var presets = [
+    {
+        num: {
+            food: 30,
+            pred: 10,
+            prey: 20
+        },
+        custom: []
+    },
+    {
+        num: {
+            food: 30,
+            fungus: 4,
+            hive: 1,
+            pred: 10,
+            prey: 20
+        },
+        custom: []
+    },
+    {
+        num: {
+            food: 30,
+            prey: 20
+        },
+        custom: []
+    },
+    {
+        num: {
+            prey: 20
+        },
+        custom: [
+            {
+                name: 'fungus',
+                x: 0,
+                y: 0 
+            },
+            {
+                name: 'fungus',
+                x: 100,
+                y: 100
+            },
+            {
+                name: 'fungus',
+                x: 200,
+                y: 200
+            },
+            {
+                name: 'fungus',
+                x: 300,
+                y: 300
+            },
+            {
+                name: 'fungus',
+                x: 400,
+                y: 400
+            }
+        ]
+    }
+];
+var currentPreset = 0;
+
+var avoidLines = false;
+var chaseLines = false;
+var lineMode = false;
+var motionBlur = false;
+var showChart = true;
+var showNutrition = true;
+var showPerception = true;
+
+var menuVisible = true;
+var sidebarOpen = true;
+
+
+// Misc functions
+
+// Set position to inside map and adjust velocity
+function bounceOffEdges(e) {
+    var dv = -4;
+    if (e.pos.x - e.radius < 0) {
+        e.pos.x = e.radius;
+        e.vel.x *= dv;
+    }
+    if (e.pos.x + e.radius > width) {
+        e.pos.x = width - e.radius;
+        e.vel.x *= dv;
+    }
+    if (e.pos.y - e.radius < 0) {
+        e.pos.y = e.radius;
+        e.vel.y *= dv;
+    }
+    if (e.pos.y + e.radius > height) {
+        e.pos.y = height - e.radius;
+        e.vel.y *= dv;
+    }
 }
- // 
+
+// Create entity at mouse position
+function drawEntity() {
+    if (sidebarOpen && mouseX < 220) return;
+    if (menuVisible && mouseX < 220 && mouseY < 30) return;
+    entities.push(createEntity(mouseX, mouseY, templates[selected]));
+}
+
+function initEntities() {
+    entities = [];
+    newEntities = [];
+    // Setup map from preset
+    var preset = presets[currentPreset];
+    var keys = Object.keys(preset.num);
+    for (var i = 0; i < keys.length; i++) {
+        var template = keys[i];
+        var count = preset.num[template];
+        for (var j = 0; j < count; j++) {
+            var x = random(width);
+            var y = random(height);
+            entities.push(createEntity(x, y, templates[template]));
+        }
+    }
+
+    // Spawn custom entities
+    for (var i = 0; i < preset.custom.length; i++) {
+        var e = preset.custom[i];
+        entities.push(createEntity(e.x, e.y, templates[e.name]));
+    }
+}
+
+function isOutsideMap(e) {
+    return isOutsideRect(e.pos.x, e.pos.y, 0, 0, width, height);
+}
+
+// Draw pie chart to show ratio of each type of entity
+function pieChart(entities) {
+    var total = getByName(entities, [
+        'food', 'prey', 'pred', 'hive', 'swarm', 'fungus'
+    ]).length;
+    var nums = [
+        getByName(entities, 'food').length,
+        getByName(entities, 'prey').length,
+        getByName(entities, ['hive', 'swarm']).length,
+        getByName(entities, 'pred').length,
+        getByName(entities, 'fungus').length,
+    ];
+    var colors = [
+        templates.food.color, templates.prey.color, templates.swarm.color,
+        templates.pred.color, templates.fungus.color
+    ];
+    
+    // Calculate angles
+    var angles = [];
+    for (var i = 0; i < nums.length; i++) {
+        angles[i] = nums[i] / total * TWO_PI;
+    }
+
+    // Draw pie chart
+    var diam = 150;
+    var lastAngle = 0;
+    for (var i = 0; i < angles.length; i++) {
+        if (angles[i] === 0) continue;
+        // Arc
+        fill(colors[i].concat(191));
+        noStroke();
+        arc(width - 100, 100, diam, diam, lastAngle, lastAngle + angles[i]);
+        lastAngle += angles[i];
+    }
+}
+
+// Clear dead entities from entities array
+function removeDead(entities) {
+    for (var i = entities.length - 1; i >= 0; i--) {
+        var e = entities[i];
+        if (e.alive) continue;
+        entities.splice(i, 1);
+        e.onDeath(newEntities);
+    }
+}
+
+function toggleMenu() {
+    sidebarOpen = !sidebarOpen;
+    var m = document.getElementById('menu');
+    if (sidebarOpen && menuVisible) {
+        m.style.display = 'block';
+    } else {
+        m.style.display = 'none';
+    }
+}
+
+
+// Main p5 functions
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    initEntities();
+}
 
 function draw() {
+    // Make background slightly transparent for motion blur
+    if (motionBlur) {
+        background(0, 63);
+    } else {
+        background(0);
+    }
 
+    // Restart if there are not too many entities or too few dynamic entities
+    var total = entities.length;
+    var numDynamic = getByName(entities, [
+        'pred', 'prey', 'swarm', 'swarmer'
+    ]).length;
+    if (total <= 0 || total > 1200 || numDynamic === 0) initEntities();
+
+    // Randomly spawn food on map
+    if (random(5) < 1) {
+        var x = random(width);
+        var y = random(height);
+        entities.push(createEntity(x, y, templates.food));
+    }
+
+    // Update and draw all entities
+    for (var i = 0; i < entities.length; i++) {
+        var e = entities[i];
+        // Steering
+        var visible = e.getVisible(entities);
+        var names = e.toChase.concat(e.toEat).concat(e.toAvoid);
+        var relevant = getByName(visible, names);
+        var f;
+        if (relevant.length === 0) {
+            f = e.wander(newEntities);
+        } else {
+            f = e.steer(relevant, newEntities);
+        }
+        // Update
+        e.applyForce(f.limit(e.accAmt));
+        e.update();
+        bounceOffEdges(e);
+        if (isOutsideMap(e)) e.kill();
+        e.hunger(newEntities);
+        // Draw
+        e.draw(lineMode, showPerception, showNutrition);
+        // Misc
+        e.onFrame(newEntities);
+        // Eating
+        var targets = getByName(visible, e.toEat);
+        for (var j = 0; j < targets.length; j++) {
+            var t = targets[j];
+            if (e.contains(t.pos.x, t.pos.y)) e.onEatAttempt(t, newEntities);
+        }
+    }
+
+    // Draw pie chart
+    if (showChart) pieChart(entities);
+
+    removeDead(entities);
+    entities = entities.concat(newEntities);
+    newEntities = [];
+}
+
+
+// Misc p5 functions
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    initEntities();
+}
+
+
+// User input
+
+function keyPressed() {
+    switch (keyCode) {
+        case 13:
+            // Enter
+            initEntities();
+            break;
+        case 17:
+            // Ctrl
+            showPerception = !showPerception;
+            break;
+        case 18:
+            // Alt
+            avoidLines = !avoidLines;
+            break;
+        case 32:
+            // Spacebar
+            chaseLines = !chaseLines;
+            break;
+        case 48:
+        case 49:
+        case 50:
+        case 51:
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+        case 56:
+        case 57:
+            // 0-9
+            var n = keyCode - 48;
+            if (currentPreset !== n && presets.length > n) {
+                currentPreset = n;
+                initEntities();
+            }
+            break;
+        case 66:
+            // B
+            selected = 'prey';
+            break;
+        case 70:
+            // F
+            selected = 'food';
+            break;
+        case 71:
+            // G
+            showChart = !showChart;
+            break;
+        case 72:
+            // H
+            selected = 'hive';
+            break;
+        case 77:
+            // M
+            lineMode = !lineMode;
+            if (lineMode) {
+                avoidLines = true;
+                chaseLines = true;
+            } else {
+                avoidLines = false;
+                chaseLines = false;
+            }
+            break;
+        case 78:
+            // N
+            showNutrition = !showNutrition;
+            break;
+        case 79:
+            // O
+            motionBlur = !motionBlur;
+            break;
+        case 80:
+            // P
+            selected = 'pred';
+            break;
+        case 81:
+            // Q
+            menuVisible = !menuVisible;
+            var b = document.getElementById('menu-button');
+            var m = document.getElementById('menu');
+            if (menuVisible) {
+                b.style.display = 'inline';
+            } else {
+                b.style.display = 'none';
+                m.style.display = 'none';
+            }
+            break;
+        case 83:
+            // S
+            selected = 'swarm';
+            break;
+        case 86:
+            // V
+            selected = 'fungus';
+            break;
+    }
+}
+
+function mousePressed() {
+    drawEntity();
+}
+
+function mouseDragged() {
+    drawEntity();
 }
